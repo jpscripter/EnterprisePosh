0d535a2f7ee8ea0fa68324f0484af7beff8f0c1b
JeffTheScripter <JPScripter@gmail.com>
2021-07-15 21:09:40 -0500

    debugging

--- Debug/Debug.ps1
+++ Debug/Debug.ps1
@@ -1,16 +1,15 @@
 
-    Set-Location E:\Repos\MMS\EnterprisePosh\Debug
-    $FilePath = "E:\RunSpaceData.txt"
-    Import-Module .\Module.psm1
+Set-Location E:\Repos\MMS\EnterprisePosh\Debug
+$FilePath = "E:\RunSpaceData.txt"
+Import-Module .\Module.psm1
 
-    $Loops = 10
-    $BreakPoint = 3
-    
-    wait-debugger
-    :LoopName For ($Counter = 0; $Counter -LT $loops; $Counter++){
-        If ($Counter -eq $BreakPoint){
-            Break LoopName
-        }
-        "Sleeping $Counter"
-        Start-UDFSleep
+$Loops = 10
+$BreakPoint = 3
+
+:LoopName For ($Counter = 0; $Counter -LT $loops; $Counter++){
+    If ($Counter -eq $BreakPoint){
+        Break LoopName
     }
+    "Sleeping $Counter"
+    Start-UDFSleep
+}
